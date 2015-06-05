516d2812d19409e43398fdbbb39b0fe9cf4af454
Lento Manickathan <lento.manickathan@gmail.com>
2015-06-05 09:48:52 +0200

    added marker option for viewing projectors and so on, made internal functions hidden. examples updated

--- examples/PlotEnv/OpenFOAM_example.py
+++ examples/PlotEnv/OpenFOAM_example.py
@@ -2,7 +2,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pyFlowStat.PointProbe import PointProbe
-plt.ion()
+#plt.ion()
 
 path=r'Data/10/U'
 pt = PointProbe()
@@ -13,19 +13,20 @@ plt.plot(pt.data['t'],pt.data['U'])
 plt.grid(True)
 plt.xlabel('Time $[s]$')
 plt.ylabel('Velocity $[m/s]$')
-plt.legend(['$U_x$','$U_y$','$U_z$'])
+plt.legend(('$U_x$','$U_y$','$U_z$'))
 plt.savefig('figureStandard.pdf')
-plt.show()
+plt.show(block=False)
 
 from pyFlowStat import plotEnv
 
 # Custom plot environment
 plotEnv.setupPlotEnv(numColors=3,style='ticks')
+
 plt.figure('Customized plot')
 plt.plot(pt.data['t'],pt.data['U'])
-plt.grid(True)
 plt.xlabel('Time $[s]$')
 plt.ylabel('Velocity $[m/s]$')
-plt.legend(['$U_x$','$U_y$','$U_z$'])
-plotEnv.cleanupFigure(despine=True, tightenFigure=True)
+plotEnv.cleanupFigure(despine=True, tightenFigure=True, addMarkers=False, 
+					  addLegend=True, labels=['$U_x$','$U_y$','$U_z$'], 
+					  legendLoc=1)
 plt.savefig('figureCustom.pdf')
